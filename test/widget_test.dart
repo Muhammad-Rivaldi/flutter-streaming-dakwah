4fe1217e2f097fbd9a45c14be7f4943b8dc5b4da
MuhammadRivaldi <mrvld.paldo@gmail.com>
2021-02-17 01:35:36 +0700

    update some code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application__streaming_video/main.dart';
+import 'package:Streaming_Dakwah/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
